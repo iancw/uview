b101568e16afe7da140cbed2a9c1a47d95768589
Ian Will <icwill@gmail.com>
2012-07-28 14:35:31 -0400

    Build the main hall balcony

--- app/assets/javascripts/toggle.js
+++ app/assets/javascripts/toggle.js
@@ -4,26 +4,58 @@ var image;
 
 var seats = [];
 
+var dims = {width:10, height:10};
+var spacing = 5;
+
+
+//main hall balcony...
 var section1 = {
-    angle: 45,
-    offsetx: 50,
+    startseat: 0,
+    sectionno: 0,
+    angle: 0,
+    offsetx: 5,
     offsety: 0,
-    numRows: 3,
-    rows: [10, 10, 10]
+    rows: [7, 5, 5, 5, 8, 8, 8, 8]
 };
 
 var section2 = {
+    startseat: 0,
+    sectionno: 1,
     angle: 0,
-    offsetx: 20,
-    offsety: 300,
-    numRows: 5,
-    rows: [15, 15, 15, 15, 15]
+    offsetx: 8*(dims.width+spacing)+2*spacing,
+    offsety: 0,
+    rows: [8, 6, 6, 12, 12, 11, 10, 9]
 }
 
-var dims = {width:20, height:20};
-var spacing = 5;
+var section3 = {
+    startseat:0,
+    sectionno:2,
+    angle: 90,
+    offsetx: section2.offsetx + 12*(dims.width+spacing) + 2*spacing,
+    offsety: section2.rows.length * (dims.height+spacing) + 2*spacing,
+    rows: [14, 10, 9]
+}
 
-var sections = new Array(section1, section2);
+var section4 = {
+    startseat:0,
+    sectionno:3,
+    angle:90,
+    offsetx: section3.offsetx,
+    offsety: section3.offsety + 14*(dims.height+spacing) + 2*spacing,
+    rows: [8, 8, 8]
+}
+
+var section5 = {
+    startseat:0,
+    sectionno:4,
+    angle:180,
+    offsetx:section4.offsetx - section4.rows.length*(dims.width+spacing) - 2*spacing,
+    offsety: section4.offsety + 4*(dims.width+spacing) + 8*(dims.width+spacing),
+    rows: [7, 7, 9, 10]
+}
+
+
+var sections = new Array(section1, section2, section3, section4, section5);
 
 var winW = 630, winH = 460;
 if (document.body && document.body.offsetWidth) {
@@ -133,7 +165,7 @@ function handlePostResponse(){
 function hardcodeSeats(){
     //Initialize all seats to 0
     for(sec=0; sec<sections.length; sec++){
-        for(r=0; r<sections[sec].numRows; r++){
+        for(r=0; r<sections[sec].rows.length; r++){
             for(iseat=0; iseat<sections[sec].rows[r]; iseat++){
                 seats[seats.length]=0;
             }
@@ -146,48 +178,80 @@ function compToHex(c){
     return hex.length == 1 ? "0" + hex : hex;
 }
 
-function pickColor(seatno){
+function pickColorSeat(seatno){
 //    alert(str.fontcolor(rgb(seatno, seatno, seatno)));
 //    return str.fontcolor(rgb(seatno, seatno, seatno));
-    return "#" + compToHex(seatno) + compToHex(seatno) + compToHex(seatno);
+    return "#" + compToHex(seatno) + compToHex(seatno) + compToHex(0);
+}
+
+function pickColorSection(filled, sectionno){
+    return "#" + compToHex(0) + compToHex(0) + compToHex(sectionno);
+}
+
+function filledColor(filled, sectionno){
+    if(filled == 0){
+        return "#00FF00";
+    }
+    return "#000000"; 
 }
 
 function realColor(seatno){
-    if(seatno < seats.length && seats[seatno]>0){
-        return "#00FF00";
-    }
-    return "#000000";
+    if(seatno < seats.length){
+       return filledColor(seats[seatno]);
+   }
+   return "#FFFFFF";
 }
 
-function drawSection(section, context, seatno, colorfunc){
+function drawSection(section, context, seatno, colorfunc, sectioncolor){
     context.translate(section.offsetx, section.offsety);
     context.rotate(section.angle * Math.PI / 180.0);
-    for(ro=0; ro<section.numRows; ro++){
+
+    section.startseat=seatno;
+    var filled=0;
+    var empty=0;
+
+    for(ro=0; ro<section.rows.length; ro++){
         for(i=0; i<section.rows[ro]; i++){
             var radius=i*(dims.width+spacing);
             context.fillStyle = colorfunc(seatno);
+            if(seats[seatno] == 0){empty++;}
+            else{ filled++; }
             context.fillRect(radius, 0, dims.width,dims.height);
             seatno++;
         }
         context.translate(0, (dims.height+spacing));
     }
+    //Make the color opposite of the majority
+    //to make it most convenient to toggle
+    
+    var sectionCol=1;
+    if(filled > empty){
+        sectionCol=0;
+    }
+    context.fillStyle=sectioncolor(sectionCol, section.sectionno);
+    context.beginPath();
+    context.arc(dims.width, dims.height,dims.width, 0,Math.PI*2,true);
+    context.closePath();
+    context.fill();
+
     return seatno;
 }
 
 // canvas image manipulation 
-function draw(drawCanv, colorfunc) {
+function draw(drawCanv, colorfunc, sectionfunc) {
     var localContext = drawCanv.getContext('2d');
     localContext.fillStyle = "#FFFFFF";
     localContext.fillRect(0, 0, drawCanv.width, drawCanv.height);
 
     var seatno=0;
     for(ix=0; ix<sections.length; ix++){
-        seatno=drawSection(sections[ix], localContext, seatno, colorfunc);
+        seatno=drawSection(sections[ix], localContext, seatno, colorfunc, sectionfunc);
         localContext.setTransform(1, 0, 0, 1, 0, 0);
     }
 }
 
 function process(evt) {
+
 	canvas = document.getElementById('seatCanvas');
     var mous = getMousePos(evt);
 
@@ -200,9 +264,8 @@ function process(evt) {
             seats[pickedseat]=0;
         }
         sendSeatsJSON(pickedseat);
-        draw(canvas, realColor);
+        draw(canvas, realColor, filledColor);
     }
-
 }
 
 function getSeat(mousePos){
@@ -212,12 +275,12 @@ function getSeat(mousePos){
     var gctx = ghostcanvas.getContext('2d');
     var data = gctx.getImageData(mousePos.x, mousePos.y, 1, 1);
 
-    return data.data[2];
+    return data.data[0];
 }
 
 function redraw(){
     canvas = document.getElementById('seatCanvas');
-    draw(canvas, realColor);
+    draw(canvas, realColor, filledColor);
 }
 
 function init() {
@@ -226,12 +289,12 @@ function init() {
     canvas.width  = canvWidth;
     canvas.height = canvHeight;
     canvas.addEventListener('click', process, false);
-    draw(canvas, realColor);
+    draw(canvas, realColor, filledColor);
 
     ghostcanvas = document.createElement('canvas');//getElementById('ghostcanvas');
     ghostcanvas.height = canvas.height;
     ghostcanvas.width = canvas.width;
-    draw(ghostcanvas, pickColor);
+    draw(ghostcanvas, pickColorSeat, pickColorSection);
 
     poll();
 }
